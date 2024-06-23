0366f9d6ea98e27dfb1369250ca812562bba8037
tomat <xxlennygamerxx@gmail.com>
2024-06-23 14:52:52 -0700

    Tweak plugin to host the log passthrough server. Also allow it to specify no proxy URL. Begin working on reimplementing logging.

--- src/Tomat.LimbusServer.Proxy/ProxyPlugin.cs
+++ src/Tomat.LimbusServer.Proxy/ProxyPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
@@ -6,6 +7,7 @@ using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 using Il2CppSystem.Text;
 using Server;
+using Tomat.LimbusServer.Passthrough.API;
 
 namespace Tomat.LimbusServer.Proxy;
 
@@ -16,10 +18,13 @@ namespace Tomat.LimbusServer.Proxy;
 internal sealed class ProxyPlugin : BasePlugin {
     private const string default_proxy_url = "http://127.0.0.1";
     private const string default_identifier_key = "DEFAULT_CONSTANT_VALUE_THIS_GENERATES_A_NEW_KEY";
-    private static readonly string[] known_urls = ["https://www.limbuscompanyapi.com", "https://www.limbuscompanyapi-2.com"];
+    private const bool default_host_log_passthrough = true;
+    private const string default_host_url = "www.limbuscompanyapi.com";
+    private static readonly string[] known_urls = [default_host_url, "https://www.limbuscompanyapi-2.com"];
 
     private static ConfigEntry<string>? proxyUrl;
     private static ConfigEntry<string>? identifierKey;
+    private static ConfigEntry<bool>? hostLogPassthrough;
     private static ManualLogSource? log;
 
     public override void Load() {
@@ -27,8 +32,10 @@ internal sealed class ProxyPlugin : BasePlugin {
 
         proxyUrl = Config.Bind("General", "Proxy URL", default_proxy_url);
         identifierKey = Config.Bind("General", "Identifier Key", default_identifier_key);
-        Log.LogInfo($"Using proxy url: {proxyUrl.Value}");
+        hostLogPassthrough = Config.Bind("General", "Host Log Passthrough", default_host_log_passthrough);
+        Log.LogInfo($"Using proxy url: {proxyUrl.Value} (host url: {GetHostUrl()})");
         Log.LogInfo($"Using identifier key: {identifierKey.Value}");
+        Log.LogInfo($"Hosting log passthrough server: {hostLogPassthrough.Value}");
 
         if (identifierKey.Value == default_identifier_key) {
             Log.LogInfo($"Generating new identifier key (because key was '{default_identifier_key}')...");
@@ -48,6 +55,19 @@ internal sealed class ProxyPlugin : BasePlugin {
 
         Log.LogInfo("Applying Harmony patches...");
         new Harmony(MyPluginInfo.PLUGIN_GUID).PatchAll(typeof(ProxyPlugin));
+
+        if (!hostLogPassthrough.Value)
+            return;
+
+        var port = new Random().Next(10000, 20000);
+        var listeningAddress = $"http://127.0.0.1:{port}/";
+
+        Task.Run(
+            () => {
+                using var server = PassthroughServer.Create([listeningAddress], "https://www.limbuscompanyapi.com");
+                server.Start();
+            }
+        );
     }
 
     // ReSharper disable once InconsistentNaming
@@ -55,10 +75,8 @@ internal sealed class ProxyPlugin : BasePlugin {
     [HarmonyPrefix]
     [HarmonyPatch(typeof(HttpApiRequester), nameof(HttpApiRequester.SendRequest))]
     private static bool PrefixSendRequest(HttpApiRequester __instance, HttpApiSchema httpApiSchema, bool isUrgent) {
-        var theProxyUrl = proxyUrl?.Value ?? default_proxy_url;
-
         foreach (var url in known_urls)
-            httpApiSchema._url = httpApiSchema._url.Replace(url, theProxyUrl);
+            httpApiSchema._url = httpApiSchema._url.Replace(url, GetHostUrl());
 
         if (!string.IsNullOrEmpty(identifierKey?.Value))
             httpApiSchema._url += $"?key={identifierKey.Value}";
@@ -67,4 +85,9 @@ internal sealed class ProxyPlugin : BasePlugin {
         return true;
     }
     // ReSharper restore UnusedParameter.Local
+
+    private static string GetHostUrl() {
+        var theProxyUrl = proxyUrl?.Value;
+        return string.IsNullOrEmpty(theProxyUrl) ? default_host_url : theProxyUrl;
+    }
 }
