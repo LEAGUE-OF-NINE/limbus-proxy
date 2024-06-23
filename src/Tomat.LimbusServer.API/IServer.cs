1e7b3eb691ad8d01a616890ac60d4028a7b0968b
tomat <xxlennygamerxx@gmail.com>
2024-06-23 13:11:45 -0700

    Begin drafting self-hostable server API.

---
+++ src/Tomat.LimbusServer.API/IServer.cs
@@ -0,0 +1,19 @@
+﻿namespace Tomat.LimbusServer.API;
+
+/// <summary>
+///     Provides a server interface for the Limbus server.
+/// </summary>
+/// <remarks>
+///     <see cref="IServerProvider"/>s should be used to initialize and start
+///     servers. As such, there is not exposed API for starting a server from
+///     the <see cref="IServer"/> instance.
+///     <br />
+///     Servers should be closed by invoking <see cref="IDisposable.Dispose"/>
+///     (preferably automatically through this dispose pattern).
+/// </remarks>
+public interface IServer : IDisposable {
+    /// <summary>
+    ///     The server provider that created this server instance.
+    /// </summary>
+    IServerProvider ServerProvider { get; }
+}
---
+++ src/Tomat.LimbusServer.API/IServerProvider.cs
@@ -0,0 +1,18 @@
+﻿namespace Tomat.LimbusServer.API;
+
+/// <summary>
+///     Provides APIs for setting up and managing an <see cref="IServer"/>.
+/// </summary>
+/// <remarks>
+///     The server provider's lifetime should not be ended until all servers
+///     managed by it have been disposed.
+/// </remarks>
+public interface IServerProvider : IDisposable {
+    /// <summary>
+    ///     Starts the server.
+    /// </summary>
+    /// <returns>
+    ///     An initialized and running <see cref="IServer"/> instance.
+    /// </returns>
+    IServer Start();
+}
